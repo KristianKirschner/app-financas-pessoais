7fe7893c4e946d2b5a60959b3a35a31a5f491a91
unknown <kristianlll@hotmail.com>
2026-04-12 15:02:51 -0300

    Card movimentações diárias

---
+++ mobile/src/components/BalanceItem/index.js
@@ -0,0 +1,31 @@
+import { useMemo } from "react";
+import { Balance, Container, Label } from "./styles";
+
+export default function BalanceItem({data}){
+
+    const labelName = useMemo(() => {
+        if (data.tag === 'saldo'){
+            return{
+                label: 'Saldo atual',
+                color: '3b3dbf'
+            }
+        } else if (data.tag === 'receita'){
+                        return{
+                label: 'Entradas de hoje',
+                color: '30b94a'
+            }
+        } else {
+            return{
+                label: 'Saídas de hoje',
+                color: '3F463a'
+            }
+        }
+    },[data])
+
+    return(
+        <Container bg={labelName.color}>
+            <Label>{labelName.label} </Label>
+            <Balance>R$ {data.saldo}</Balance>
+        </Container>
+    )
+}
--- mobile/src/pages/Home/index.js
+++ mobile/src/pages/Home/index.js
@@ -1,13 +1,46 @@
-import { useContext } from "react";
-import { AuthContext } from "../../contexts/auth";
-import { Background } from './styles';
+import { Background, ListBalance } from './styles';
 import Header from "../../components/Header";
+import { useEffect, useState } from 'react';
+import api from '../../services/api'
+import {format} from 'date-fns'
+import { useIsFocused } from '@react-navigation/native';
+import BalanceItem from '../../components/BalanceItem';
 
 export default function Home(){
+
+    const [listBalance, setListBalance] = useState([]);
+    const [dateMovements, setDateMovements] = useState(new Date());
+    const isFocused = useIsFocused();
+
+    useEffect(()=>{
+        let isActive = true;
+
+        async function getMovements() {
+            let dateFormated = format(dateMovements, 'dd/MM/yyyy')
+            const balance = await api.get('/balance', {
+                params: {
+                    date: dateFormated
+                }
+            })
+            if (isActive){
+            setListBalance(balance.data)
+            }
+        }
+        getMovements();
+        return () => isActive = false;
+    },[isFocused])
 
     return(
         <Background>
             <Header title="Minhas movimentações" ></Header>
+            <ListBalance
+                data={listBalance}
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                keyExtractor={item => item.tag}
+                renderItem={({item}) => (<BalanceItem data={item} />)}
+            />
+
         </Background>
     )
 }
